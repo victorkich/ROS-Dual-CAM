27db2aebe0a1ace297e8b52d0cc529ce3224cf89
victorkich <victorkich@yahoo.com.br>
2022-03-06 12:53:42 -0300

    Update

--- dual-cam.py
+++ dual-cam.py
@@ -5,22 +5,21 @@ import cv2
 import threading
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
+from utils.defisheye import Defisheye
 import time
 
 
 class camThread(threading.Thread):
-    def __init__(self, previewName, camID):
+    def __init__(self, camID=0):
         threading.Thread.__init__(self)
-        self.previewName = previewName
         self.camID = camID
 
     def run(self):
-        print("Starting " + self.previewName)
-        camPreview(self.previewName, self.camID)
+        print("Starting camera:", self.camID)
+        camPreview(self.camID)
 
 
-def camPreview(previewName, camID):
-    # cv2.namedWindow(previewName)
+def camPreview(camID):
     pub_image = rospy.Publisher('/usb_cam/image_raw_{}'.format(camID), Image, queue_size=30)
     cam = cv2.VideoCapture(camID)
     cam.set(cv2.CAP_PROP_BUFFERSIZE, 2)
@@ -33,22 +32,22 @@ def camPreview(previewName, camID):
 
     while rval:
         start = time.time()
-        # cv2.imshow(previewName, frame)
         rval, frame = cam.read()
+        frame = defisheye.convert(frame)
         frame = bridge.cv2_to_imgmsg(frame)
         pub_image.publish(frame)
-        fps = round(1 / (time.time() - start), 2)
+        fps = round(1 / (time.time() - start), 1)
         print('FPS:', fps)
-        #key = cv2.waitKey(20)
-        #if key == 27:  # exit on ESC
-        #    break
-    #cv2.destroyWindow(previewName)
+        key = cv2.waitKey(1)
+        if key == 27:  # exit on ESC
+            break
 
 
-rospy.init_node('cameras')
+rospy.init_node('camera_processing')
 bridge = CvBridge()
+defisheye = Defisheye(dtype='linear', format='fullframe', fov=100, pfov=90)
 # Create two threads as follows
-thread1 = camThread("Camera 1", 0)
-thread2 = camThread("Camera 2", 2)
+thread1 = camThread(camID=0)
+thread2 = camThread(camID=2)
 thread1.start()
 thread2.start()
