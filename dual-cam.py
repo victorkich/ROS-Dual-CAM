5edfeae6a07b84167343cd05b69b03f664b2c4aa
victorkich <victorkich@yahoo.com.br>
2022-03-11 15:47:05 -0300

    Update

--- dual-cam.py
+++ dual-cam.py
@@ -8,41 +8,6 @@ from sensor_msgs.msg import Image
 from utils.defisheye import Defisheye
 import time
 
-"""
-class camThread(threading.Thread):
-    def __init__(self, camID=0):
-        threading.Thread.__init__(self)
-        self.camID = camID
-
-    def run(self):
-        print("Starting camera:", self.camID)
-        camPreview(self.camID)
-
-
-def camPreview(camID):
-    pub_image = rospy.Publisher('/usb_cam/image_raw_{}'.format(camID), Image, queue_size=30)
-    cam = cv2.VideoCapture(camID)
-    cam.set(cv2.CAP_PROP_BUFFERSIZE, 2)
-    cam.set(cv2.CAP_PROP_FPS, 30)
-    cam.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
-    if cam.isOpened():
-        rval, frame = cam.read()
-    else:
-        rval = False
-
-    while rval:
-        start = time.time()
-        rval, frame = cam.read()
-        frame = defisheye.convert(frame)
-        frame = bridge.cv2_to_imgmsg(frame)
-        pub_image.publish(frame)
-        fps = round(1 / (time.time() - start), 1)
-        print('FPS:', fps)
-        key = cv2.waitKey(1)
-        if key == 27:  # exit on ESC
-            break
-"""
-
 rospy.init_node('camera_processing')
 bridge = CvBridge()
 defisheye = Defisheye(dtype='linear', format='fullframe', fov=180, pfov=120)
