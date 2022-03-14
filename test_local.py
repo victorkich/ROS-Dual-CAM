5d103df1721aaa6f1513803c7fe693fd071bc583
victorkich <victorkich@yahoo.com.br>
2022-03-14 16:00:00 -0300

    Update

--- test_local.py
+++ test_local.py
@@ -13,14 +13,15 @@ class TestLocal:
         self.bridge = CvBridge()
         self.image_right = None
         self.image_left = None
+        self.defisheye = Defisheye(dtype='linear', format='fullframe', fov=100, pfov=90)
         rospy.Subscriber('/usb_cam/compressed/image_right', CompressedImage, self.image_right_callback)
         rospy.Subscriber('/usb_cam/compressed/image_left', CompressedImage, self.image_left_callback)
 
     def image_right_callback(self, msg):
-        self.image_right = self.bridge.compressed_imgmsg_to_cv2(msg)
+        self.image_right = self.defisheye.convert(self.bridge.compressed_imgmsg_to_cv2(msg))
 
     def image_left_callback(self, msg):
-        self.image_left = self.bridge.compressed_imgmsg_to_cv2(msg)
+        self.image_left = self.defisheye.convert(self.bridge.compressed_imgmsg_to_cv2(msg))
 
     def step(self):
         if self.image_right is None or self.image_left is None:
