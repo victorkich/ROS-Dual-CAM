73a485315f9936971dc03df3def2a390d41de9d3
victorkich <victorkich@yahoo.com.br>
2023-02-06 10:43:00 -0300

    Update

--- test_local.py
+++ test_local.py
@@ -5,7 +5,6 @@ import cv2
 from cv_bridge import CvBridge
 from sensor_msgs.msg import CompressedImage
 from utils.defisheye import Defisheye
-# from utils.panorama import Stitcher
 from utils import range_finder as rf
 import numpy as np
 import time
@@ -20,42 +19,37 @@ class TestLocal:
         self.stitcher = cv2.Stitcher_create()
         self.image_right = None
         self.image_left = None
-        self.defisheye1 = Defisheye(dtype='linear', format='fullframe', fov=140, pfov=110)  # 140 110
+        # self.defisheye1 = Defisheye(dtype='linear', format='fullframe', fov=140, pfov=110)  # 140 110
         self.defisheye2 = Defisheye(dtype='linear', format='fullframe', fov=180, pfov=80)  # 180 80
         rospy.Subscriber('/camera_right/image_raw/compressed', CompressedImage,  self.image_right_callback, tcp_nodelay=True, queue_size=1, buff_size=2**26)
-        rospy.Subscriber('/camera_left/image_raw/compressed', CompressedImage, self.image_left_callback, tcp_nodelay=True, queue_size=1, buff_size=2**26)
+        # rospy.Subscriber('/camera_left/image_raw/compressed', CompressedImage, self.image_left_callback, tcp_nodelay=True, queue_size=1, buff_size=2**26)
 
     def image_right_callback(self, msg):
         # self.image_right = self.defisheye1.convert(self.bridge.compressed_imgmsg_to_cv2(msg))
         self.image_right = self.bridge.compressed_imgmsg_to_cv2(msg)
 
-    def image_left_callback(self, msg):
-        # self.image_left = self.defisheye2.convert(self.bridge.compressed_imgmsg_to_cv2(msg))
-        self.image_left = self.bridge.compressed_imgmsg_to_cv2(msg)
+    # def image_left_callback(self, msg):
+    #    # self.image_left = self.defisheye2.convert(self.bridge.compressed_imgmsg_to_cv2(msg))
+    #    self.image_left = self.bridge.compressed_imgmsg_to_cv2(msg)
 
     def step(self):
         if self.image_right is None or self.image_left is None:
-            return False
-        #defish_left = self.defisheye1.convert(self.image_left)
-        #defish_right = self.defisheye2.convert(self.image_right)
+            return False, None
+        # defish_left = self.defisheye1.convert(self.image_left)
+        # defish_right = self.defisheye2.convert(self.image_right)
         # frame = self.stitcher.stitch([defish_left, defish_right])
-        #(status, frame) = self.stitcher.stitch([defish_left, defish_right])
+        # (status, frame) = self.stitcher.stitch([defish_left, defish_right])
         # frame = [self.defisheye1.convert(self.image_left), self.defisheye2.convert(self.image_right)]
         # size = self.image_right.shape
         # frame = self.image_right
         # frame = frame[round(size[0]*0.1):round(size[0]*0.9), round(size[1]*0.28):round(size[1]*0.68)]
         # frame = frame[round(size[0]*0.1):round(size[0]*0.8), round(size[1]*0.1):round(size[1]*0.6)]
-        # lidar = np.zeros(24)
-        # angle, distance, frame = real_ttb.get_angle_distance(frame, lidar, green_magnitude=1.0)
-        # print('Angle:', angle, 'Distance:', distance)
-        # frame = self.defisheye1.convert(self.image_left)
-        #print('Frame 1 shape:', defish_left.shape, 'Frame 2 shape:', defish_right.shape)
-        #print('Status:', status)
-        #if status:
-        #    print('Output frame:', frame.shape)
-        frame = self.image_right
+        frame = self.defisheye2.convert(self.image_right)
+        lidar = np.zeros(24)
+        angle, distance, frame = real_ttb.get_angle_distance(frame, lidar, green_magnitude=1.0)
+        print('Angle:', angle, 'Distance:', distance)
         cv2.imshow('Frame', frame)
-        return True
+        return True, frame
 
 
 # Loading configs from config.yaml
@@ -63,19 +57,30 @@ path = os.path.dirname(os.path.abspath(__file__))
 with open(path + '/config.yml', 'r') as ymlfile:
     config = yaml.load(ymlfile, Loader=yaml.FullLoader)
 
-real_ttb = rf.RealTtb(config, path, output=(640, 640))
+
+# Recording settings
+cap = cv2.VideoCapture(0)
+cap.set(3, 720)
+cap.set(4, 720)
+fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (720, 720))
+
+real_ttb = rf.RealTtb(config, path, output=(720, 720))
 rospy.init_node('test_local')
 test_local = TestLocal()
 key = cv2.waitKey(1)
 while key != ord('q'):
     start = time.time()
-    val = test_local.step()
+    val, frame = test_local.step()
     if not val:
         continue
+    out.write(frame)
     key = cv2.waitKey(1)
     time.sleep(1/70)
     fps = round(1 / (time.time() - start), 1)
     print('\rFPS:', fps)
 
 print("[INFO] cleaning up...")
+cap.release()
+out.release()
 cv2.destroyAllWindows()
